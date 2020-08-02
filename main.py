52eaae7add40a9164f0e6fbcec0095484b715499
67Samuel <60543441+67Samuel@users.noreply.github.com>
2020-08-02 15:29:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -380,7 +380,8 @@ def test(model, test_loader, criterion):
             data, target = data.to(device), target.to(device)
             output = model(data)
             num_correct_k += get_topk(output, target, k=args.topk)
-            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+            #test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += criterion(output, target).item()
             pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
             correct += pred.eq(target.data.view_as(pred)).sum().item()
         test_loss /= len(test_loader.dataset)
