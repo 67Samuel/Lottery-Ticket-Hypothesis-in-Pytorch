5cc09aa34921b1fea58ed9e3f55f1de388eeb2d2
67Samuel <60543441+67Samuel@users.noreply.github.com>
2020-08-03 15:20:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -440,7 +440,7 @@ if __name__=="__main__":
 
 
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
-    os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu
+    #os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu
     
     
     #FIXME resample
